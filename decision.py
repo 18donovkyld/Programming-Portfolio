50acb273b4c132e63a1f8577c405595670046308
Kylie Donovan <kyliedonovan@Alveyworld-02.local>
2015-09-14 10:18:08 -0600

    Examples from book

---
+++ decision.py
@@ -0,0 +1,14 @@
+# Learning about if statements
+
+number = int(raw_input("Enter your age: "))
+if number < 5: 
+	print "You are not in school yet"
+elif number < 11:
+	print "You are in Elementary School"
+elif number < 15:		
+	print "You are still in Middle/Intermediate School"
+elif number < 19:
+	print "You are in High School"	
+else: 
+	print "You know everything"	
+	
---
+++ learningpython2.py
@@ -0,0 +1,8 @@
+# Second Practice with python
+
+fahrenheit = 0.0 
+print "Fahrenheit Celsius"
+while fahrenheit <= 250:
+	celcius = ( fahrenheit - 32.0 ) / 1.8
+	print "%5.1f %7.2f" % (fahrenheit , celsius)
+	fahrenheit = fahrenheit + 25
